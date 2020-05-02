621d6790a5ba62a221233bc926423a3c6d3f986b
TiGeorge <georgetikhomirov@mail.ru>
2020-05-02 16:41:20 +0300

    Initial commit

---
+++ src/observer/entities/WeatherData.java
@@ -0,0 +1,42 @@
+package observer.entities;
+
+import observer.Observer;
+import observer.Subject;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WeatherData implements Subject {
+
+    private Set<Observer> observers = new HashSet<>();
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observers.forEach(observer->observer.update(temperature, humidity, pressure));
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+
+        measurementsChanged();
+    }
+}
